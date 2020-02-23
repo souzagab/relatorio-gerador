f2755f1bf7b5ae621bf08ddc030f82c21ee2ecd7
Gabriel S <desouza.gabriel@icloud.com>
2020-02-23 18:31:49 -0300

    feat: 🎸 Create generic report module
    Created via scaffolding a generic module of Reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
-  
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
 
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  resources :reports
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200223210907_create_reports.rb
@@ -0,0 +1,17 @@
+class CreateReports < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reports do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :company, limit: 255
+      t.string :institution, limit: 255
+      t.string :type, limit: 30
+      t.date :report_date
+      t.integer :hours_per_day, limit: 2
+      t.string :month, limit: 30
+      t.integer :working_days, limit: 2
+      t.string :activities, limit: 10000
+
+      t.timestamps
+    end
+  end
+end
