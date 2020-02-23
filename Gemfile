316ff59efab1e04fc43a68f7334078cad16f8cfe
Gabriel S <desouza.gabriel@icloud.com>
2020-02-23 18:56:39 -0300

    refactor: 💡 Refactor reports module, adding decorators

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'draper'
 gem 'devise'
 gem 'omniauth-google-oauth2'
 # Use Active Storage variant
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   root 'home#index'
   get 'home/index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :reports
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
