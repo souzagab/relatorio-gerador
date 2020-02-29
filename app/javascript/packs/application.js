1076eaf17fba002dc330d585f28ac28ff1aef50f
Gabriel S <desouza.gabriel@icloud.com>
2020-02-29 02:09:15 -0300

    feat(Adiciona AdminLTE, Bootstrap 4): Adiciona AdminLTE , Bootstrap 4 e suas dependências, cria temp

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+var jQuery = require("jquery")
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+require('admin-lte')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
