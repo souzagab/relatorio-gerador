69842d11117c0e1827a06a648c4f32da855dd17e
Gabriel S <desouza.gabriel@icloud.com>
2020-05-03 13:16:13 -0300

    feat(dependencies): add toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-var jQuery = require("jquery")
+
+const jQuery = require("jquery")
+global.toastr = require("toastr")
 // import jQuery from "jquery";
 global.$ = global.jQuery = jQuery;
+
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
 require('admin-lte')
-
+import "../stylesheets/application.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
